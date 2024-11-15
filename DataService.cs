9e8ac5d7e3cfdca12785c056a4f1ee997bd7095b
uliloewi <zequanyu@yahoo.de>
2024-11-15 16:23:42 +0100

    取韻腳字

---
+++ DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shang4Gu3In1
+{
+    public class DataService
+    {
+        public static HttpClient Client = new HttpClient()
+        {
+            Timeout = Timeout.InfiniteTimeSpan
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Shang4Gu3In1;
+using Aspose.Cells;
+using System.Text;
+
+namespace zhongguliin
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpResponseMessage = await DataService.Client.GetAsync("http://www.kaom.net/yayuns_bu88.php?book=%E6%A5%9A%E8%BE%AD%E9%9F%BB&x=%E9%AD%9A&y=%E9%AD%9A&mode=yunbu");
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+            ProcessTable(content);
+        }
+
+        private static void ProcessTable(string theText)
+        {
+            string[] lines = theText.Split(    new string[] { Environment.NewLine },    StringSplitOptions.None);
+            string table = lines.Where(x=>x.StartsWith("<table><tr><th")).FirstOrDefault();
+            lines = table.Split(new string[] { "<tr><td>" }, StringSplitOptions.None);
+            foreach (string line in lines)
+            {
+                var rythms = line.Split(new string[] { "<b style=\"" }, StringSplitOptions.None);
+                if (rythms.Length > 1)
+                {
+
+                    for (int i = 0; i < rythms.Length-1; i++)
+                    {
+                        string zy = rythms[i].Substring(rythms[i].Length - 1);
+                        Console.Write(zy);
+                    }
+                    Console.WriteLine();
+                }
+            } 
+        }
+    }    
+}
