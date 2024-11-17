de8a9b93d8c576422b5efa84ae585caefcf51ad4
uliloewi <zequanyu@yahoo.de>
2024-11-17 20:12:30 +0100

    給韻腳找出柳漫的上古擬音

--- Program.cs
+++ Program.cs
@@ -8,12 +8,18 @@ namespace zhongguliin
     {
         static async Task Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             var httpResponseMessage = await DataService.Client.GetAsync("http://www.kaom.net/yayuns_bu88.php?book=%E6%A5%9A%E8%BE%AD%E9%9F%BB&x=%E9%AD%9A&y=%E9%AD%9A&mode=yunbu");
             var content = await httpResponseMessage.Content.ReadAsStringAsync();
-            ProcessTable(content);
+
+            Workbook wk = new Workbook("./shang4gu3li3in1.xlsx");
+            Worksheet ws = wk.Worksheets[0];
+            //var dt = ws.Cells.ExportDataTable(0, 0, 10000, 15);
+            //var shangguin = ws.Cells["D1"].Value.ToString();
+            ProcessTable(content, ws);
         }
 
-        private static void ProcessTable(string theText)
+        private static void ProcessTable(string theText, Worksheet ws)
         {
             string[] lines = theText.Split(    new string[] { Environment.NewLine },    StringSplitOptions.None);
             string table = lines.Where(x=>x.StartsWith("<table><tr><th")).FirstOrDefault();
@@ -28,6 +34,11 @@ namespace zhongguliin
                     {
                         string zy = rythms[i].Substring(rythms[i].Length - 1);
                         Console.Write(zy);
+                        for (int j = 1; j < 9915; j++)
+                        {
+                            if (ws.Cells["L" + j.ToString()].Value.ToString().Contains(zy))
+                                Console.Write(ws.Cells["D" + j.ToString()].Value.ToString() + "/");
+                        }
                     }
                     Console.WriteLine();
                 }
